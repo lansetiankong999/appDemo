82dc5cde4d3d78076d137edb584adfc6636bebc0
何孔祥 <c-hekongxiang@genomics.cn>
2020-08-13 14:57:01 +0800

    2.2.1

--- server/gateway/gateway-web/src/test/java/com/springboot/cloud/gateway/GatewayApplicationTests.java
+++ server/gateway/gateway-web/src/test/java/com/springboot/cloud/gateway/GatewayApplicationTests.java
@@ -4,15 +4,21 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class GatewayApplicationTests {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     @Test
     public void contextLoads() {
-
+        System.out.println(stringRedisTemplate.opsForValue().get("gateway_routes::103"));
     }
 }
