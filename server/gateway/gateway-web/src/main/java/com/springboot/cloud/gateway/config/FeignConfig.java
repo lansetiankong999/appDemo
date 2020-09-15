67a4eb6d249d3a86e3ea6b1f2ddf1f031d9a1253
何孔祥 <c-hekongxiang@genomics.cn>
2020-09-15 18:20:01 +0800

    add FeignConfig

---
+++ server/gateway/gateway-web/src/main/java/com/springboot/cloud/gateway/config/FeignConfig.java
@@ -0,0 +1,41 @@
+package com.springboot.cloud.gateway.config;
+
+import feign.codec.Decoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Jump
+ * @date 2020/9/15 18:15
+ */
+
+@SpringBootConfiguration
+public class FeignConfig {
+
+    @Bean
+    public Decoder feignDecoder() {
+        return new ResponseEntityDecoder(new SpringDecoder(feignHttpMessageConverter()));
+    }
+
+    public ObjectFactory<HttpMessageConverters> feignHttpMessageConverter() {
+        final HttpMessageConverters httpMessageConverters = new HttpMessageConverters(new GateWayMappingJackson2HttpMessageConverter());
+        return () -> httpMessageConverters;
+    }
+
+    public static class GateWayMappingJackson2HttpMessageConverter extends MappingJackson2HttpMessageConverter {
+        GateWayMappingJackson2HttpMessageConverter() {
+            List<MediaType> mediaTypes = new ArrayList<>();
+            mediaTypes.add(MediaType.valueOf(MediaType.TEXT_HTML_VALUE + ";charset=UTF-8"));
+            setSupportedMediaTypes(mediaTypes);
+        }
+    }
+}
